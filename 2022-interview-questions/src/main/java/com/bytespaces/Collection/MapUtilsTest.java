b5c277e71d93f5aaa6a1e7e3e617c78e80e9fdce
Annicko <18296768912@139.com>
2023-04-13 16:58:17 +0800

    test

---
+++ 2022-interview-questions/src/main/java/com/bytespaces/Collection/MapUtilsTest.java
@@ -0,0 +1,23 @@
+package com.bytespaces.Collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Copyright (C), 2010-2022,
+ * FileName:     MapUtilsTest
+ * Author:       shine
+ * Date:         2023/3/6 15:51
+ * Description:
+ * History:
+ * <author>      <time>          <version>          <desc>
+ * 作者姓名       修改时间         版本号             描述
+ */
+public class MapUtilsTest {
+
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+        map.put("one","test");
+        map.get(null);
+    }
+}
